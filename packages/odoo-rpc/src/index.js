854b6c2b68ea456307398e16737e3dbf1aedbe75
suxue <1683248243@qq.com>
2019-01-02 11:44:11 +0800

    错误捕获

--- packages/odoo-rpc/src/index.js
+++ packages/odoo-rpc/src/index.js
@@ -1,106 +1,114 @@
-import modelCreator from './models'
-import _RPC from './rpc'
+import modelCreator from './models';
+import _RPC from './rpc';
 
-import busCreator from './bus.bus'
-import mailChannelCreator from './mail.channel'
-import mailMessageCreator from './mail.message'
+import busCreator from './bus.bus';
+import mailChannelCreator from './mail.channel';
+import mailMessageCreator from './mail.message';
 
-const create_env = ({models, creators, rpc, odoo} )=>{
-    const env = {}
-    for(const mdl in models ){
-        const fields = models[mdl]
-        const creator = creators[mdl] || modelCreator
-        const cls = creator({model: mdl, fields, rpc, env, odoo })
-        env[mdl] = cls
-    }
+const create_env = ({ models, creators, rpc, odoo }) => {
+  const env = {};
+  for (const mdl in models) {
+    const fields = models[mdl];
+    const creator = creators[mdl] || modelCreator;
+    const cls = creator({ model: mdl, fields, rpc, env, odoo });
+    env[mdl] = cls;
+  }
 
-    return env
-}
+  return env;
+};
 
 class Odoo {
-    constructor(options){
-        //console.log('parant odoo',options)
-        const { host, db, models={}, creators={}, RPC } = options
-        const RPC0 = RPC || _RPC
-        const rpc = new RPC0({ host,db })
-        this._rpc = rpc
-        this._models = models
+  constructor(options) {
+    //console.log('parant odoo',options)
+    const {
+      host,
+      db,
+      models = {},
+      creators = {},
+      RPC,
+      sid,
+      errrCallback,
+    } = options;
+    const RPC0 = RPC || _RPC;
+    const rpc = new RPC0({ host, db, sid, errrCallback });
+    this._rpc = rpc;
+    this._models = models;
 
-        const base_creators = {
-            'bus.bus': busCreator ,
-            'mail.channel': mailChannelCreator ,
-            'mail.message': mailMessageCreator
-        }
+    const base_creators = {
+      'bus.bus': busCreator,
+      'mail.channel': mailChannelCreator,
+      'mail.message': mailMessageCreator,
+    };
 
-        this._env = create_env({
-            models,
-            creators: { ...base_creators, ...creators },
-            rpc,
-            odoo: this
+    this._env = create_env({
+      models,
+      creators: { ...base_creators, ...creators },
+      rpc,
+      odoo: this,
+    });
+  }
 
-        })
+  setErrorCallback(callback) {
+    // to set callback function, if odoo request, error response
+    this._rpc.callbackerror = callback;
+  }
+
+  async init() {
+    // to init all cls , so cls is used safely
+    // TBD, only request one time
+    for (const model in this._env) {
+      await this._env[model].init();
     }
+  }
 
-    setErrorCallback(callback){
-        // to set callback function, if odoo request, error response
-        this._rpc.callbackerror = callback
+  async login(params) {
+    const data = await this._rpc.login(params);
+    if (!data.code) {
+      Odoo._session[this._rpc.sid] = this;
+      await this.init();
+      return this._rpc.sid;
     }
+    return null;
+  }
 
-    async init(){
-        // to init all cls , so cls is used safely
-        // TBD, only request one time
-        for (const model in this._env){
-            await this._env[model].init()
-        }
+  async logout() {
+    const sid = this._rpc.sid;
+    const data = this._rpc.logout();
+    delete Odoo._session[sid];
+    return data;
+  }
+
+  env(model) {
+    // get a model cls from odoo._env
+    let cls = this._env[model];
+    if (!cls) {
+      cls = modelCreator({ model, rpc: this._rpc, env: this._env });
+      this._env[model] = cls;
     }
+    return cls;
+  }
 
-    async login(params){
-        const data = await this._rpc.login(params )
-        if(!data.code){
-            Odoo._session[this._rpc.sid] = this
-            await this.init()
-            return this._rpc.sid
-        }
-        return null
-    }
+  async user(fields) {
+    // get login user
+    const uid = this._rpc.uid;
+    return this.env('res.users').browse(uid, fields);
+  }
 
-    async logout(){
-        const sid = this._rpc.sid
-        const data = this._rpc.logout()
-        delete Odoo._session[sid]
-        return data
-    }
+  me = this.user;
 
-    env(model){
-        // get a model cls from odoo._env
-        let cls = this._env[model]
-        if(!cls){
-            cls = modelCreator({ model, rpc: this._rpc, env:this._env })
-            this._env[model] = cls
-        }
-        return cls
-    }
-
-    async user(fields) {
-        // get login user
-        const uid = this._rpc.uid
-        return this.env('res.users').browse(uid,fields)
-    }
-
-    me = this.user
-
-    async ref(xmlid) {
-        // get model and id from xmlid
-        return this.env('ir.model.data').call('xmlid_to_res_model_res_id', [xmlid, true] )
-    }
+  async ref(xmlid) {
+    // get model and id from xmlid
+    return this.env('ir.model.data').call('xmlid_to_res_model_res_id', [
+      xmlid,
+      true,
+    ]);
+  }
 }
 
-Odoo._session = {}
+Odoo._session = {};
 
-Odoo.load = (session_id) =>{
-    return Odoo._session[session_id]
-}
+Odoo.load = session_id => {
+  return Odoo._session[session_id];
+};
 
-
-
-export default Odoo
+export default Odoo;
--- packages/odoo-rpc/src/rpc.js
+++ packages/odoo-rpc/src/rpc.js
@@ -1,194 +1,205 @@
-
 // Only For Debug
-import fetch from 'dva/fetch';
-
 const _fetch = (url, options, timeout) => {
-    return Promise.race([
-        fetch(url, options),
-        new Promise(function (resolve, reject) {
-            setTimeout(() => reject(new Error('request timeout')), timeout);
-        })
-    ]);
-}
-
+  return Promise.race([
+    fetch(url, options),
+    new Promise(function(resolve, reject) {
+      setTimeout(() => reject(new Error('request timeout')), timeout);
+    }),
+  ]);
+};
 
 const checkStatus = response => {
-    if (response.status >= 200 && response.status < 300) {
-        return response;
-    }
-    const error = new Error(response.statusText);
-    error.name = response.status;
-    error.data = response;
-    throw error;
-    return null
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.name = response.status;
+  error.data = response;
+  throw error;
+  return null;
 };
 
 const checkJsonrpc = async (response, oid, options) => {
-    const data = await response.json()
-    const { id, jsonrpc } = data
-    if (id === oid && jsonrpc === '2.0') {
-        return data
-    }
-    const error = new Error('Error jsonrpc');
-    error.name = 'Jsonrpc';
-    error.data = { id: oid, options, data };
-    throw error;
-    return null
+  const data = await response.json();
+  const { id, jsonrpc } = data;
+  if (id === oid && jsonrpc === '2.0') {
+    return data;
+  }
+  const error = new Error('Error jsonrpc');
+  error.name = 'Jsonrpc';
+  error.data = { id: oid, options, data };
+  throw error;
+  return null;
 };
 
 const checkOdooError = data => {
-    const { result, error: error0 } = data
-    if (!error0) {
-        return result
-    }
+  const { result, error: error0 } = data;
+  if (!error0) {
+    return result;
+  }
 
-    const { code, message, data: data2 } = error0
-    const error = new Error(message);
-    error.name = code;
-    error.data = data2;
-    throw error;
-    return null
+  const { code, message, data: data2 } = error0;
+  const error = new Error(message);
+  error.name = code;
+  error.data = data2;
+  throw error;
+  return null;
 };
 
 const jsonrpc = (url, params, timeout = 120) => {
-    //console.log('jsonrpc=',url, params)
-    const id = Math.round(Math.random() * 1000000000)
-    const options = {
-        method: 'POST',
-        body: JSON.stringify({
-            jsonrpc: 2.0,
-            id,
-            method: 'call',
-            params: params,
-        }),
-        //headers: new Headers({ 'Content-Type': 'application/json' })
-        headers: { 'content-type': 'application/json' }
-    };
+  //console.log('jsonrpc=',url, params)
+  const id = Math.round(Math.random() * 1000000000);
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      jsonrpc: 2.0,
+      id,
+      method: 'call',
+      params: params,
+    }),
+    //headers: new Headers({ 'Content-Type': 'application/json' })
+    headers: { 'content-type': 'application/json' },
+  };
 
-    const myFetch = (timeout == 0) ? fetch : _fetch
-    const args = (timeout == 0) ? [url, options] : [url, options, timeout * 1000]
+  const myFetch = timeout == 0 ? fetch : _fetch;
+  const args = timeout == 0 ? [url, options] : [url, options, timeout * 1000];
 
-    return myFetch(...args)
-        .then(res => {
-            //  console.log('1st',res)
-            return checkStatus(res)
-        })
-        .then(async (res) => {
-            // console.log('after status',res )
-            return checkJsonrpc(res, id, options)
-        })
-        .then(data => {
-            // console.log( 'after jsonrpc', data)
-            return checkOdooError(data)
-        })
-        .then(result => {
-            // console.log( 'result ok', result)
-            return { code: 0, result }
-
-        })
-        .catch(error => {
-            return {
-                code: 1, error
-            }
-        })
-}
+  return myFetch(...args)
+    .then(res => {
+      //  console.log('1st',res)
+      return checkStatus(res);
+    })
+    .then(async res => {
+      // console.log('after status',res )
+      return checkJsonrpc(res, id, options);
+    })
+    .then(data => {
+      // console.log( 'after jsonrpc', data)
+      return checkOdooError(data);
+    })
+    .then(result => {
+      // console.log( 'result ok', result)
+      return { code: 0, result };
+    })
+    .catch(error => {
+      return {
+        code: 1,
+        error,
+      };
+    });
+};
 
 class RPC {
-    constructor(options) {
-        const { host = '/api', db, sid, uid, timeout = 120 } = options
-        this.host = host
-        this.db = db
-        this.timeout = timeout
-        this.sid = null
-        this.uid = null
+  constructor(options) {
+    const {
+      host = '/api',
+      db,
+      sid = null,
+      uid,
+      timeout = 120,
+      callbackerror = () => false,
+    } = options;
+    this.host = host;
+    this.db = db;
+    this.timeout = timeout;
+    this.sid = sid;
+    this.uid = null;
+    this._callbackerror = callbackerror || this._callbackerror;
+  }
 
+  _callbackerror(url, params, error) {
+    console.log(url, params, error);
+  }
+  async verifySid(sid) {
+    //从localStorage取sid，向后台发送验证，
+  }
+  async json(url, params, timeout) {
+    const timeout1 = timeout == undefined ? this.timeout : timeout;
+    const data = await jsonrpc(url, params, timeout1);
+    const { code, error } = data;
+    //console.log(url, params, data)
+
+    if (code) {
+      console.log(url, params, error);
+      this._callbackerror(url, params, error);
+      //console.log(url, params, error)
+    }
+    return data;
+  }
+
+  async login(params) {
+    const { db, login, password } = params;
+    const url = `${this.host}/json/user/login`;
+
+    if (db) {
+      this.db = db;
     }
 
+    const data = await this.json(url, {
+      login,
+      password,
+      db: this.db,
+      type: 'account',
+    });
 
-    callbackerror( url,params,error) {
-        console.log(url,params,error)
+    const { code } = data;
+    if (!code) {
+      const {
+        result: { status },
+      } = data;
+      if (status == 'ok') {
+        const {
+          result: { sid, uid },
+        } = data;
+        this.sid = sid;
+        this.uid = uid;
+      } else {
+        this.sid = null;
+        this.uid = null;
+      }
+    } else {
+      this.sid = null;
+      this.uid = null;
     }
 
-    async json(url, params, timeout) {
-        const timeout1 = (timeout == undefined) ? this.timeout : timeout
-        const data = await jsonrpc(url, params, timeout1)
-        const { code, error } = data
-        //console.log(url, params, data)
+    return data;
+  }
 
-        if (code) {
-            console.log(url, params, error)
-            this.callbackerror(url,params,error)
-            //console.log(url, params, error)
-        }
-        return data
+  async logout() {
+    if (!this.sid) {
+      return { code: 1, error: {} };
+    }
+    const url = `${this.host}/web/session/destroy?session_id=${this.sid}`;
+    const data = await this.json(url, {});
+    const { code } = data;
+    if (!code) {
+      this.sid = null;
+      this.uid = null;
     }
 
-    async login(params) {
-        const { db, login, password } = params
-        const url = `${this.host}/json/user/login`
+    this.sid = null;
+    this.uid = null;
 
-        if (db) {
-            this.db = db
-        }
+    return data;
+  }
 
-        const data = await this.json(url, { login, password, db: this.db, type: 'account' })
-
-        const { code } = data
-        if (!code) {
-            const { result: { status } } = data
-            if (status == 'ok') {
-                const { result: { sid, uid } } = data
-                this.sid = sid
-                this.uid = uid
-            }
-            else {
-                this.sid = null
-                this.uid = null
-            }
-        }
-        else {
-            this.sid = null
-            this.uid = null
-        }
-
-        return data
+  async call(params) {
+    const { model, method, args = [], kwargs = {} } = params;
+    const url = `${this.host}/json/api?session_id=${this.sid}`;
+    if (!this.sid) {
+      this._callbackerror(url, params, (error = { message: 'no sid' }));
+      return { code: 1, error: { message: 'no sid' } };
+    }
+    const data = await this.json(url, { model, method, args, kwargs });
+    const { code } = data;
+    if (!code) {
+      const { result } = data;
     }
 
-    async logout() {
-        if (!this.sid) {
-            return { code: 1, error: {} }
-        }
-        const url = `${this.host}/web/session/destroy?session_id=${this.sid}`
-        const data = await this.json(url, {})
-        const { code } = data
-        if (!code) {
-            this.sid = null
-            this.uid = null
-        }
+    return data;
+  }
 
-        this.sid = null
-        this.uid = null
-
-        return data
-    }
-
-    async call(params) {
-        if (!this.sid) {
-            return { code: 1, error: { message: 'no sid' } }
-        }
-
-        const { model, method, args = [], kwargs = {} } = params
-        const url = `${this.host}/json/api?session_id=${this.sid}`
-        const data = await this.json(url, { model, method, args, kwargs })
-        const { code } = data
-        if (!code) {
-            const { result } = data
-        }
-
-        return data
-    }
-
-    /*
+  /*
     async longpoll(params) {
         if (!this.sid) {
             return { code: 1, error: { message: 'no sid' } }
@@ -202,8 +213,6 @@ class RPC {
         return data
     }
    */
-
 }
 
-export default RPC
-
+export default RPC;
